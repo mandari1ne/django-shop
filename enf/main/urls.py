773523360d578be32e99f01811a6386d04d3cc97
mandari1ne <marina.stakhovets@gmail.com>
2025-09-03 23:50:22 +0300

    creating and including urls

---
+++ enf/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# for namespace generation
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('catalog/', views.CatalogView.as_view(), name='category_all'),
+    path('catalog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
+    path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+]
