6d5f81abc62fcd36def3f4d60fa67864fef30bec
mandari1ne <marina.stakhovets@gmail.com>
2025-09-11 16:38:44 +0300

    create cart middleware

--- enf/enf/settings.py
+++ enf/enf/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'cart.middleware.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'enf.urls'
@@ -69,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
                 'cart.context_processors.cart_processor'
             ],
         },
